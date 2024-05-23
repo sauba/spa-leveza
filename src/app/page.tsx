900b3fcba6552c257b01ba641a8342724bde971c
Thiago José de Queiroz <42189768+sauba@users.noreply.github.com>
2024-05-23 00:31:32 -0300

    Atualizar o page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-gradient-to-br from-purple-700 via-purple-600 to-purple-400 items-center justify-center">
       <Header />
-      <Image src={`/logo.svg`} width={300} height={300} alt="Leveza Para Viver logo" className={`min-h-screen animate-pulse cursor-none py-16`} />
+      <Image src={`/logo.svg`} width={300} height={300} alt="Leveza Para Viver logo" className={`animate-pulse cursor-none py-16`} />
       <About />
     </main>
   );
