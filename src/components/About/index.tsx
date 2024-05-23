aa01f4055c960ed22dc474b5d48604dbdc0543c4
Thiago José de Queiroz <42189768+sauba@users.noreply.github.com>
2024-05-22 23:28:30 -0300

    Atualizar o index.tsx

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -14,7 +14,7 @@ export default function About() {
         text-white
         font-caveat
       `} id="about">
-      <p className={`px-24 text-center lg:text-4xl xl:text-5xl 2xl:text-6xl tracking-wider leading-loose`}>
+      <p className={`px-24 text-center lg:text-3xl xl:text-4xl 2xl:text-6xl tracking-wider leading-loose`}>
         O Leveza Para Viver é a manifestação de uma filosofia de vida, da Mulher e Terapeuta, Daniele Andrade. Um espaço de evolução que coloca em movimento, diferentes projetos terapêuticos, te apresentando caminhos para uma vida com mais Leveza Para Viver.
       </p>
     </div>
