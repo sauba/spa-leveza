37e1ad2a2cd9634edffb5849fe1907c84d5ebad2
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-06-04 14:21:27 -0300

    Update: LeveTarot component

--- src/components/LeveTarot/index.tsx
+++ src/components/LeveTarot/index.tsx
@@ -1,4 +1,4 @@
-import { Card } from "@nextui-org/card";
+import { Accordion, AccordionItem } from "@nextui-org/react";
 
 export default function LeveTarot() {
   return (
@@ -13,35 +13,29 @@ export default function LeveTarot() {
         text-center
         outline-none
         border-none
-        text-white
+        bg-zinc-300
         font-caveat
       `} id="levetarot">
-      <Card className={`card`}>
-        <p className={`card-header px-4 lg:px-24 text-center text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl tracking-wider`}>
-          O que é o Tarot Terapêutico ?
-        </p>
+      <div>
 
-        <p className={`px-4 lg:px-24 text-center text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl tracking-wider`}>
-          O Tarot é uma ferramenta de autoconhecimento, que representa um mistério para muitos de nós. Nada que também não aconteça com a própria temática ‘autoconhecimento’, que, culturalmente, foi tão afastada de nós... Mas, relaxa, nós estamos aqui, justamente, em prol dessa merecida reconexão!
-        </p>
-      </Card>
-
-
-      <p className={`px-4 lg:px-24 text-center text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl tracking-wider`}>
-        Como Experienciar o Tarot Terapêutico ?
-      </p>
-
-      <p className={`px-4 lg:px-24 text-center text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl tracking-wider`}>
-        Uma Tiragem de Tarot acontece no AGORA… é claro que podem vir cartas que te ajudem na compreensão de energias do passado, e, certamente, você colherá conselhos e orientações para o futuro, mas, é importante perceber, que as mensagens estão se manifestando no presente, fortalecendo seu processo de autoconhecimento e sua tomada de consciência no agora!
-      </p>
-
-      <p className={`px-4 lg:px-24 text-center text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl tracking-wider`}>
-        Quando Recorrer ao Tarot Terapêutico ?
-      </p>
-
-      <p className={`px-4 lg:px-24 text-center text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl tracking-wider`}>
-        Sempre que estiver em busca de clareza e apoio para suas intenções, ações, decisões… o Tarot será bem-vindo, te beneficiando com as revelações que já estão disponíveis para você.
-      </p>
+      </div>
+      <Accordion>
+        <AccordionItem indicator key="1" aria-label="O que é o Tarot Terapêutico ?" title="O que é o Tarot Terapêutico ?" className={`text-2xl xl:text-6xl`} >
+          <p className={`px-4 lg:px-24 text-center text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl tracking-wider`}>
+            O Tarot é uma ferramenta de autoconhecimento, que representa um mistério para muitos de nós. Nada que também não aconteça com a própria temática ‘autoconhecimento’, que, culturalmente, foi tão afastada de nós... Mas, relaxa, nós estamos aqui, justamente, em prol dessa merecida reconexão!
+          </p>
+        </AccordionItem>
+        <AccordionItem indicator key="2" aria-label="Como Experienciar o Tarot Terapêutico ?" title="Como Experienciar o Tarot Terapêutico ?" className={`text-2xl xl:text-6xl`} >
+          <p className={`px-4 lg:px-24 text-center text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl tracking-wider`}>
+            Uma Tiragem de Tarot acontece no AGORA… é claro que podem vir cartas que te ajudem na compreensão de energias do passado, e, certamente, você colherá conselhos e orientações para o futuro, mas, é importante perceber, que as mensagens estão se manifestando no presente, fortalecendo seu processo de autoconhecimento e sua tomada de consciência no agora!
+          </p>
+        </AccordionItem>
+        <AccordionItem indicator key="3" aria-label="Quando Recorrer ao Tarot Terapêutico ?" title="Quando Recorrer ao Tarot Terapêutico ?" className={`text-2xl xl:text-6xl`}>
+          <p className={`px-4 lg:px-24 text-center text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl tracking-wider`}>
+            Sempre que estiver em busca de clareza e apoio para suas intenções, ações, decisões… o Tarot será bem-vindo, te beneficiando com as revelações que já estão disponíveis para você.
+          </p>
+        </AccordionItem>
+      </Accordion>
     </div>
   )
 }
