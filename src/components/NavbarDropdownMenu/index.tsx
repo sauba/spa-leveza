111f2f96e33e38b94e13a8279c36a7da86aa272a
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-06-05 19:30:16 -0300

    Update: NavbarDropdown bg

--- src/components/NavbarDropdownMenu/index.tsx
+++ src/components/NavbarDropdownMenu/index.tsx
@@ -27,7 +27,7 @@ export default function NavbarDropdownMenu() {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Menu.Items className="origin-top-left absolute left-0 w-64 bg-transparent">
+              <Menu.Items className="origin-top-left absolute left-0 w-64 bg-lime-700 xl:bg-transparent">
                 <div className="flex-col justify-start items-start">
                   <Menu.Item>
                     {({ active }) => (
@@ -48,7 +48,7 @@ export default function NavbarDropdownMenu() {
                             flex
                             items-center
                             px-4
-                            pb-2
+                            py-2
                             text-sm
                             hover:text-lg
                           `}
