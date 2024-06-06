7b9425eccdba65814bef2c887b566b0ad76735c5
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-06-06 02:48:33 -0300

    Update: fix tarot

--- src/pages/servicos/tarot/index.tsx
+++ src/pages/servicos/tarot/index.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import ModalidadesTarot from "@/components/ModalidadesTarot";
-import "./styles";
-import { LeveTarotContainer } from "./styles";
 
 export default function ModalidadesTarotPage() {
   return (
