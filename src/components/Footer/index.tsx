bd4480ad9f2b9432b05909b34ed7bf08f3d7c4a2
Thiago José de Queiroz <42189768+sauba@users.noreply.github.com>
2024-06-07 02:13:30 -0300

    Atualizar o index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
       className={`min-h-screen flex-col justify-start text-zinc-700 items-center text-center`}
       id="contato"
     >
-      <div className={`relative flex flex-col justify-center items-center mx-auto`}>
+      <div className={`flex flex-col justify-center items-center mx-auto`}>
         <span
           className={`text-4xl lg:text-6xl text-center font-allura text-sky-700 xl:pt-12`}
         >
@@ -53,7 +53,7 @@ export default function Footer() {
         </div>
       </div>
 
-      <svg className={`relative flex justify-center items-center mx-auto rounded-full overflow-hidden z-10 -mt-36`} version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 1600 900">
+      <svg className={`flex justify-center items-center mx-auto rounded-full overflow-hidden z-10`} version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 1600 900">
         <defs>
           <linearGradient id="bg" x2="0%" y2="100%">
             <stop offset="0%" style={{ stopColor: 'rgba(93, 150, 242, 0.6)' }}></stop>
