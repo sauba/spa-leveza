3e10494f26c009862eba35a23a1a137da62ab7f7
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-06-21 02:25:43 -0300

    Update: LeveTarot text sizes

--- src/components/LeveTarot/index.tsx
+++ src/components/LeveTarot/index.tsx
@@ -22,18 +22,18 @@ export default function LeveTarot() {
       </div>
 
       <Accordion>
-        <AccordionItem indicator key="1" aria-label="O que é o Tarot Terapêutico ?" title={<span style={{ color: '#f0f0f0' }}>O que é o Tarot Terapêutico ?</span>} className={`text-3xl xl:text-6xl font-pt-mono`}>
-          <p className={`px-2 lg:px-20 text-center text-2xl 2xl:text-3xl tracking-widest font-pt-mono`}>
+        <AccordionItem indicator key="1" aria-label="O que é o Tarot Terapêutico ?" title={<span style={{ color: '#f0f0f0', fontSize: '32px' }}>O que é o Tarot Terapêutico ?</span>} className={`text-3xl xl:text-6xl font-pt-mono`}>
+          <p className={`px-2 lg:px-20 text-center text-xl 2xl:text-2xl tracking-wider font-pt-mono`}>
             O Tarot é uma ferramenta de autoconhecimento, que representa um mistério para muitos de nós. Nada que também não aconteça com a própria temática ‘autoconhecimento’, que, culturalmente, foi tão afastada de nós... Mas, relaxa, nós estamos aqui, justamente, em prol dessa merecida reconexão!
           </p>
         </AccordionItem>
-        <AccordionItem indicator key="2" aria-label="Como Experienciar o Tarot Terapêutico ?" title={<span style={{ color: '#f0f0f0' }}>Como Experienciar o Tarot Terapêutico ?</span>} className={`text-3xl xl:text-6xl font-pt-mono`} >
-          <p className={`px-2 lg:px-20 text-center text-2xl 2xl:text-3xl tracking-widest font-pt-mono`}>
+        <AccordionItem indicator key="2" aria-label="Como Experienciar o Tarot Terapêutico ?" title={<span style={{ color: '#f0f0f0', fontSize: '32px' }}>Como Experienciar o Tarot Terapêutico ?</span>} className={`text-3xl xl:text-6xl font-pt-mono`} >
+          <p className={`px-2 lg:px-20 text-center text-xl 2xl:text-2xl tracking-wider font-pt-mono`}>
             Uma Tiragem de Tarot acontece no AGORA… é claro que podem vir cartas que te ajudem na compreensão de energias do passado, e, certamente, você colherá conselhos e orientações para o futuro, mas, é importante perceber, que as mensagens estão se manifestando no presente, fortalecendo seu processo de autoconhecimento e sua tomada de consciência no agora!
           </p>
         </AccordionItem>
-        <AccordionItem indicator key="3" aria-label="Quando Recorrer ao Tarot Terapêutico ?" title={<span style={{ color: '#f0f0f0' }}>Quando Recorrer ao Tarot Terapêutico ?</span>} className={`text-3xl xl:text-6xl font-pt-mono`}>
-          <p className={`px-2 lg:px-20 text-center text-2xl 2xl:text-3xl tracking-widest font-pt-mono`}>
+        <AccordionItem indicator key="3" aria-label="Quando Recorrer ao Tarot Terapêutico ?" title={<span style={{ color: '#f0f0f0', fontSize: '32px' }}>Quando Recorrer ao Tarot Terapêutico ?</span>} className={`text-3xl xl:text-6xl font-pt-mono`}>
+          <p className={`px-2 lg:px-20 text-center text-xl 2xl:text-2xl tracking-wider font-pt-mono`}>
             Sempre que estiver em busca de clareza e apoio para suas intenções, ações, decisões… O Tarot será bem-vindo, te beneficiando com as revelações que já estão disponíveis para você.
           </p>
         </AccordionItem>
