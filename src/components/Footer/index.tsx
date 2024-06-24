f1cc81878fc0a483aa3e6f115d215e87130758a1
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-06-24 19:47:36 -0300

    Update: Footer v3

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer
-      className={`w-full xl:min-h-screen flex-col justify-center text-zinc-700 items-center text-center font-pt-mono`}
+      className={`w-full flex-col justify-center items-center text-center font-pt-mono`}
       id="contato"
     >
-      <div className={`flex flex-col justify-center items-center mx-auto`}>
+      <div className={`w-full flex flex-col justify-center items-center mx-auto`}>
         <span
           className={`text-4xl lg:text-6xl text-center font-allura text-sky-700 pt-32`}
         >
@@ -37,7 +37,6 @@ export default function Footer() {
               </Link>
             </span>
           </div>
-
           <div className={``}>
             <span className={`text-sky-700`}>
               <Link
@@ -50,30 +49,29 @@ export default function Footer() {
             </span>
           </div>
         </div>
-      </div>
 
-      <svg className={`flex w-full justify-center items-center mx-auto rounded-full overflow-hidden z-10`} version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" height="50%" viewBox="0 0 1600 900">
-        <defs>
-          <linearGradient id="bg" x2="0%" y2="50%">
-            <stop offset="0%" style={{ stopColor: 'rgba(93, 150, 242, 0.6)' }}></stop>
-            <stop offset="100%" style={{ stopColor: 'rgba(38, 89, 190, 0.06)' }}></stop>
-          </linearGradient>
-          <path id="wave" fill="url(#bg)" d="M-363.852,502.589c0,0,236.988-41.997,505.475,0
+        <svg className={`w-full xl:h-1/4 justify-center items-center mx-auto overflow-hidden z-10 xl:-mt-96`} version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 1600 900">
+          <defs>
+            <linearGradient id="bg" x2="0%" y2="100%">
+              <stop offset="0%" style={{ stopColor: 'rgba(93, 150, 242, 0.6)' }}></stop>
+              <stop offset="100%" style={{ stopColor: 'rgba(38, 89, 190, 0.06)' }}></stop>
+            </linearGradient>
+            <path id="wave" fill="url(#bg)" d="M-363.852,502.589c0,0,236.988-41.997,505.475,0
       s371.981,38.998,575.971,0s293.985-39.278,505.474,5.859s493.475,48.368,716.963-4.995v560.106H-363.852V502.589z"></path>
-        </defs>
-        <g>
-          <use xlinkHref="#wave" opacity=".3">
-            <animateTransform attributeName="transform" attributeType="XML" type="translate" dur="8s" calcMode="spline" values="270 230; -334 180; 270 230" keyTimes="0; .5; 1" keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0" repeatCount="indefinite"></animateTransform>
-          </use>
-          <use xlinkHref="#wave" opacity=".6">
-            <animateTransform attributeName="transform" attributeType="XML" type="translate" dur="6s" calcMode="spline" values="-270 230;243 220;-270 230" keyTimes="0; .6; 1" keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0" repeatCount="indefinite"></animateTransform>
-          </use>
-          <use xlinkHref="#wave" opacity=".9">
-            <animateTransform attributeName="transform" attributeType="XML" type="translate" dur="4s" calcMode="spline" values="0 230;-140 200;0 230" keyTimes="0; .4; 1" keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0" repeatCount="indefinite"></animateTransform>
-          </use>
-        </g>
-      </svg>
-
+          </defs>
+          <g>
+            <use xlinkHref="#wave" opacity=".3">
+              <animateTransform attributeName="transform" attributeType="XML" type="translate" dur="8s" calcMode="spline" values="270 230; -334 180; 270 230" keyTimes="0; .5; 1" keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0" repeatCount="indefinite"></animateTransform>
+            </use>
+            <use xlinkHref="#wave" opacity=".6">
+              <animateTransform attributeName="transform" attributeType="XML" type="translate" dur="6s" calcMode="spline" values="-270 230;243 220;-270 230" keyTimes="0; .6; 1" keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0" repeatCount="indefinite"></animateTransform>
+            </use>
+            <use xlinkHref="#wave" opacity=".9">
+              <animateTransform attributeName="transform" attributeType="XML" type="translate" dur="4s" calcMode="spline" values="0 230;-140 200;0 230" keyTimes="0; .4; 1" keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0" repeatCount="indefinite"></animateTransform>
+            </use>
+          </g>
+        </svg>
+      </div>
       <p className="font-bad-script text-center text-sky-700">Leveza Para Viver © 2024 Todos os direitos reservados.</p>
     </footer>
   )
