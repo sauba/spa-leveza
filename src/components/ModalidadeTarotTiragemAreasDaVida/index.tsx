9dda6e66ad78b10320949c014fb3c227a613710b
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-06-25 01:56:27 -0300

    Update: NavbarDropdown v8

--- src/components/ModalidadeTarotTiragemAreasDaVida/index.tsx
+++ src/components/ModalidadeTarotTiragemAreasDaVida/index.tsx
@@ -21,17 +21,17 @@ export default function ModalidadeTarotTiragemAreasDaVida() {
         hover:cursor-pointer
         hover:rounded-br-3xl
       `} id="tiragemareasdavida">
-      <h1 className={`text-center text-4xl xl:text-8xl xl:pb-4`}>Tiragem 7 Áreas da Vida</h1>
-      <p className={`text-center text-xl xl:text-2xl px-4 font-pt-mono`}>
+      <h1 className={`text-center text-5xl xl:text-8xl xl:pb-4`}>Tiragem 7 Áreas da Vida</h1>
+      <p className={`text-center text-lg xl:text-2xl px-4 font-pt-mono`}>
         Nessa leitura olhamos para sete aspectos de sua vida, trazendo a energia das áreas pessoal, profissional, amor, família, saúde, espiritual e emocional.
       </p>
-      <p className={`text-center text-xl xl:text-2xl px-4 font-pt-mono`}>
+      <p className={`text-center text-lg xl:text-2xl px-4 font-pt-mono`}>
         Excelente para você que busca ter uma visão sistêmica de si, contribuindo para um movimento mais coeso, pois você contempla as diferentes partes que compõem sua vida e como cada uma pode estar reverberando no seu sentir.
       </p>
-      <p className={`flex justify-center items-center gap-3 text-center font-pt-mono text-xl xl:text-2xl px-4`}>
+      <p className={`flex justify-center items-center gap-3 text-center font-pt-mono text-lg xl:text-2xl px-4`}>
         <Clock size={48} weight="thin" />Duração de 1 hora;
       </p>
-      <p className={`flex justify-center items-center gap-3 text-center font-pt-mono text-xl xl:text-2xl px-4`}>
+      <p className={`flex justify-center items-center gap-3 text-center font-pt-mono text-lg xl:text-2xl px-4`}>
         <MonitorPlay size={48} weight="thin" /> online, em tempo real ou gravada;
       </p>
     </div>
