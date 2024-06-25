d3f4c49fab21837474f0983068adbf8c6cf26e90
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-06-25 02:17:20 -0300

    Update: NavbarDropdown v10

--- src/components/ModalidadeTarotTiragemLivre/index.tsx
+++ src/components/ModalidadeTarotTiragemLivre/index.tsx
@@ -19,13 +19,13 @@ export default function ModalidadeTarotTiragemLivre() {
         hover:cursor-pointer
       `} id="tiragemlivre">
       <h1 className={`text-center text-5xl xl:text-8xl xl:pb-4`}>Tiragem Livre</h1>
-      <p className={`text-center text-xl xl:text-2xl px-4 font-pt-mono`}>
+      <p className={`text-center text-lg xl:text-2xl px-4 font-pt-mono`}>
         Essa é uma leitura para você que está cheia de perguntas específicas e quer ter a liberdade de contemplar as reflexões que o Tarot traz em resposta a cada uma delas.
       </p>
-      <p className={`flex justify-center items-center gap-3 text-center font-pt-mono text-xl xl:text-2xl px-4`}>
+      <p className={`flex justify-center items-center gap-3 text-center font-pt-mono text-lg xl:text-2xl px-4`}>
         <Clock size={48} weight="thin" />Duração de 1 hora;
       </p>
-      <p className={`flex justify-center items-center gap-3 text-center font-pt-mono text-xl xl:text-2xl px-4`}>
+      <p className={`flex justify-center items-center gap-3 text-center font-pt-mono text-lg xl:text-2xl px-4`}>
         <MonitorPlay size={48} weight="thin" /> online, em tempo real ou gravada;
       </p>
     </div>
