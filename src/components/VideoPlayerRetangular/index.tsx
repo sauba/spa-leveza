8a98322f8a5a215cec13d6a3107b9addfd47780b
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-07-14 20:42:53 -0300

    Update: LeveTarot v7

---
+++ src/components/VideoPlayerRetangular/index.tsx
@@ -0,0 +1,33 @@
+// import { useEffect, useRef } from "react";
+
+export default function VideoPlayerRetangular() {
+  // const videoEl = useRef(null);
+
+  // const attemptPlay = () => {
+  //   videoEl &&
+  //     videoEl.current &&
+  //     videoEl.current.play().catch(error => {
+  //       console.error("Erro ao iniciar video", error);
+  //     });
+  // };
+
+  // useEffect(() => {
+  //   attemptPlay();
+  // }, []);
+
+  return (
+    <div className="w-full min-h-screen videoplayer relative z-10">
+      <div className={`w-full min-h-screen`}>
+        <video
+          style={{ maxWidth: "100%", maxHeight: "100%", margin: "0 auto" }}
+          playsInline
+          autoPlay
+          loop
+          muted
+          src="/video01.mp4"
+        // ref={videoEl}
+        />
+      </div>
+    </div>
+  );
+}
