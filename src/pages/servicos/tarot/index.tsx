f13d81892414e073577d1760fe6bde1abe2e2970
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-07-15 02:13:53 -0300

    Update: LeveTarot Title text-own-roxo v9

--- src/pages/servicos/tarot/index.tsx
+++ src/pages/servicos/tarot/index.tsx
@@ -4,6 +4,7 @@ import ModalidadeTarotTiragemLunar from "@/components/ModalidadeTarotTiragemLuna
 import NavbarDropdownMenuPages from "@/components/NavbarDropdownPages";
 import { WhatsappLogo } from "@phosphor-icons/react";
 import Link from "next/link";
+import "./styles.css";
 
 export default function LeveTarotPage() {
   return (
