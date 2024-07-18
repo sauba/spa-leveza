53d84d8f1fca4cbd6e36b32c9376fa0b4be41058
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-07-18 10:35:40 -0300

    Update: Saiba Mais LeveTarot v10

--- src/pages/servicos/tarot/index.tsx
+++ src/pages/servicos/tarot/index.tsx
@@ -1,5 +1,5 @@
 import NavbarDropdownMenuPages from "@/components/NavbarDropdownPages";
-import { WhatsappLogo } from "@phosphor-icons/react";
+import { Clock, WhatsappLogo } from "@phosphor-icons/react";
 import Image from "next/image";
 import Link from "next/link";
 import "./styles.css";
@@ -21,11 +21,11 @@ export default function LeveTarotPage() {
 
           <div className={`card-body bg-zinc-100 xl:h-96`}>
             <p className={`text-center text-6xl text-zinc-400 pt-1 font-bold square-peg-regular pb-2`}>Perguntas</p>
-            <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Essa tiragem é pra você que tem perguntas específicas. Você pode fazer até 5 perguntas e as cartas serão tiradas nesta energia.</p>
+            <p className={`text-center xl:text-2xl text-zinc-400 px-1 font-pt-mono`}>Essa tiragem é pra você que tem perguntas específicas. Você pode fazer até 5 perguntas e as cartas serão tiradas nesta energia.</p>
           </div>
 
           <div className={`card-footer bg-zinc-100 rounded-b-lg`}>
-            <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Duração: aproximadamente 45 minutos.</p>
+            <p className={` flex justify-center items-center gap-3 text-center xl:text-2xl text-zinc-400 px-1 py-4 font-pt-mono`}><Clock />aproximadamente 45 minutos.</p>
           </div>
         </div>
 
@@ -36,11 +36,11 @@ export default function LeveTarotPage() {
 
           <div className={`card-body bg-zinc-100 xl:h-96`}>
             <p className={`text-center text-6xl text-zinc-400 pt-1 font-bold square-peg-regular pb-2`}>Áreas da Vida</p>
-            <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Nesse formato, tiramos as cartas para fazer a leitura da energia da sua área pessoal, profissional, amorosa, familiar, financeira, de saúde e espiritual.</p>
+            <p className={`text-center xl:text-2xl text-zinc-400 px-1 font-pt-mono`}>Nesse formato, tiramos as cartas para fazer a leitura da energia da sua área pessoal, profissional, amorosa, familiar, financeira, de saúde e espiritual.</p>
           </div>
 
           <div className={`card-footer bg-zinc-100 rounded-b-lg`}>
-            <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Duração: aproximadamente 1 hora.</p>
+            <p className={` flex justify-center items-center gap-3 text-center xl:text-2xl text-zinc-400 px-1 py-4 font-pt-mono`}><Clock />aproximadamente 1 hora.</p>
           </div>
         </div>
 
@@ -51,12 +51,12 @@ export default function LeveTarotPage() {
 
           <div className={`card-body bg-zinc-100 xl:h-96`}>
             <p className={`text-center text-6xl text-zinc-400 pt-1 font-bold square-peg-regular pb-2`}>Cruz Celta</p>
-            <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Um modelo em que nos debruçamos sobre uma situação, observando o que está a atravessando, o que está posto, que se tem conhecimento, o que está oculto, questões do passado e futuro recentes relacionados a situação, atitude atual, como os  outros interagem com esta questão, seus medos/esperanças e o possível resultado dessa situação vivida.</p>
+            <p className={`text-center xl:text-2xl text-zinc-400 px-1 font-pt-mono`}>Um modelo em que nos debruçamos sobre uma situação, observando o que está a atravessando, o que está posto, que se tem conhecimento, o que está oculto, questões do passado e futuro recentes relacionados a situação, atitude atual, como os  outros interagem com esta questão, seus medos/esperanças e o possível resultado dessa situação vivida.</p>
 
           </div>
 
           <div className={`card-footer bg-zinc-100 rounded-b-lg`}>
-            <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Duração: aproximadamente 1 hora e 30 minutos.</p>
+            <p className={` flex justify-center items-center gap-3 text-center xl:text-2xl text-zinc-400 px-1 py-4 font-pt-mono`}><Clock />aproximadamente 1 hora e 30 minutos.</p>
           </div>
         </div>
 
@@ -67,11 +67,11 @@ export default function LeveTarotPage() {
 
           <div className={`card-body bg-zinc-100 xl:h-96`}>
             <p className={`text-center text-6xl text-zinc-400 pt-1 font-bold square-peg-regular pb-2`}>Ciclo Lunar</p>
-            <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Aqui, tiramos as cartas trazendo a previsão do seu ciclo lunar. Olhamos pras 4 fases (lua nova, lua crescente, lua cheia e lua minguante), trazendo a energia de cada uma e observando como as sugestões se relacionam. Excelente pra quem busca clareza, planejamento e ação. É possível fazer algumas perguntas durante a leitura.</p>
+            <p className={`text-center xl:text-2xl text-zinc-400 px-1 font-pt-mono`}>Aqui, tiramos as cartas trazendo a previsão do seu ciclo lunar. Olhamos pras 4 fases (lua nova, lua crescente, lua cheia e lua minguante), trazendo a energia de cada uma e observando como as sugestões se relacionam. Excelente pra quem busca clareza, planejamento e ação. É possível fazer algumas perguntas durante a leitura.</p>
           </div>
 
           <div className={`card-footer bg-zinc-100 rounded-b-lg`}>
-            <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Duração: aproximadamente 1 hora e 30 minutos.</p>
+            <p className={` flex justify-center items-center gap-3 text-center xl:text-2xl text-zinc-400 px-1 py-4 font-pt-mono`}><Clock />aproximadamente 1 hora e 30 minutos.</p>
           </div>
         </div>
 
@@ -82,11 +82,11 @@ export default function LeveTarotPage() {
 
           <div className={`card-body bg-zinc-100 xl:h-96`}>
             <p className={`text-center text-6xl text-zinc-400 pt-1 font-bold square-peg-regular pb-2`}>Ano Novo Pessoal</p>
-            <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Essa é sua tiragem de aniversário, feita a partir de sua data de aniversário, com a manifestação da energia pra cada um dos meses do seu novo ciclo, até a data do próximo.</p>
+            <p className={`text-center xl:text-2xl text-zinc-400 px-1 font-pt-mono`}>Essa é sua tiragem de aniversário, feita a partir de sua data de aniversário, com a manifestação da energia pra cada um dos meses do seu novo ciclo, até a data do próximo.</p>
           </div>
 
           <div className={`card-footer bg-zinc-100 rounded-b-lg`}>
-            <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Duração: aproximadamente 1 hora e 30 minutos.</p>
+            <p className={` flex justify-center items-center gap-3 text-center xl:text-2xl text-zinc-400 px-1 py-4 font-pt-mono`}><Clock />aproximadamente 1 hora e 30 minutos.</p>
           </div>
         </div>
 
@@ -97,11 +97,11 @@ export default function LeveTarotPage() {
 
           <div className={`card-body bg-zinc-100 xl:h-96`}>
             <p className={`text-center text-6xl text-zinc-400 pt-1 font-bold square-peg-regular pb-2`}>Tarot Astrológico</p>
-            <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Essa é uma leitura mais ampla. Ela traz um olhar sobre seus aspectos naturais, olhando sua energia de ação, material, mental e emocional. E, segue contemplando as 12 casas astrológicas. Cada casa astrológica é representada por um signo, que, por sua vez, manifesta uma temática da nossa vida. Observação: não é mapa astral e não precisa ter mapa astral.</p>
+            <p className={`text-center xl:text-2xl text-zinc-400 px-1 font-pt-mono`}>Essa é uma leitura mais ampla. Ela traz um olhar sobre seus aspectos naturais, olhando sua energia de ação, material, mental e emocional. E, segue contemplando as 12 casas astrológicas. Cada casa astrológica é representada por um signo, que, por sua vez, manifesta uma temática da nossa vida. Observação: não é mapa astral e não precisa ter mapa astral.</p>
           </div>
 
           <div className={`card-footer bg-zinc-100 rounded-b-lg`}>
-            <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Duração: aproximadamente 2 horas.</p>
+            <p className={` flex justify-center items-center gap-3 text-center xl:text-2xl text-zinc-400 px-1 py-4 font-pt-mono`}><Clock />aproximadamente 2 horas.</p>
           </div>
         </div>
 
