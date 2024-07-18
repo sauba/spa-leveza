0cc6223b2959941f68b20e8cb1e0e7d575622410
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-07-18 09:46:04 -0300

    Update: Saiba Mais LeveTarot v5

--- src/pages/servicos/tarot/index.tsx
+++ src/pages/servicos/tarot/index.tsx
@@ -11,18 +11,20 @@ export default function LeveTarotPage() {
 
       <h1 className={`text-center text-8xl square-peg-regular`}>Leve Tarot</h1>
 
+      <Image src={`/danicartas.svg`} width={200} height={200} className={`rounded-full mx-auto hover:transition-transform hover:scale-110 hover:cursor-none`} priority alt="" />
+
       <div className={`grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5 gap-3`}>
         <div className={`card m-8`}>
           <div className={`card-header`}>
             <Image src={`/01.svg`} width={168} height={120} className={`w-full rounded-t-xl mx-auto`} priority alt="" />
           </div>
 
-          <div className={`card-body bg-zinc-100 h-80`}>
-            <p className={`text-center text-4xl text-zinc-400 pt-1 font-bold square-peg-regular`}>Perguntas</p>
+          <div className={`card-body bg-zinc-100 h-96`}>
+            <p className={`text-center text-4xl text-zinc-400 pt-1 font-bold square-peg-regular pb-2`}>Perguntas</p>
             <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Essa tiragem é pra você que tem perguntas específicas. Você pode fazer até 5 perguntas e as cartas serão tiradas nesta energia.</p>
           </div>
 
-          <div className={`card-footer`}>
+          <div className={`card-footer bg-zinc-100 rounded-b-lg`}>
             <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Duração: aproximadamente 45 minutos.</p>
           </div>
         </div>
@@ -32,12 +34,12 @@ export default function LeveTarotPage() {
             <Image src={`/02.svg`} width={168} height={120} className={`w-full rounded-t-xl mx-auto`} priority alt="" />
           </div>
 
-          <div className={`card-body bg-zinc-100 h-80`}>
-            <p className={`text-center text-4xl text-zinc-400 pt-1 font-bold square-peg-regular`}>Áreas da Vida</p>
+          <div className={`card-body bg-zinc-100 h-96`}>
+            <p className={`text-center text-4xl text-zinc-400 pt-1 font-bold square-peg-regular pb-2`}>Áreas da Vida</p>
             <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Nesse formato, tiramos as cartas para fazer a leitura da energia da sua área pessoal, profissional, amorosa, familiar, financeira, de saúde e espiritual.</p>
           </div>
 
-          <div className={`card-footer`}>
+          <div className={`card-footer bg-zinc-100 rounded-b-lg`}>
             <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Duração: aproximadamente 1 hora.</p>
           </div>
         </div>
@@ -47,13 +49,13 @@ export default function LeveTarotPage() {
             <Image src={`/03.svg`} width={168} height={120} className={`w-full rounded-t-xl mx-auto`} priority alt="" />
           </div>
 
-          <div className={`card-body bg-zinc-100 h-80`}>
-            <p className={`text-center text-4xl text-zinc-400 pt-1 font-bold square-peg-regular`}>Cruz Celta</p>
+          <div className={`card-body bg-zinc-100 h-96`}>
+            <p className={`text-center text-4xl text-zinc-400 pt-1 font-bold square-peg-regular pb-2`}>Cruz Celta</p>
             <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Um modelo em que nos debruçamos sobre uma situação, observando o que está a atravessando, o que está posto, que se tem conhecimento, o que está oculto, questões do passado e futuro recentes relacionados a situação, atitude atual, como os  outros interagem com esta questão, seus medos/esperanças e o possível resultado dessa situação vivida.</p>
 
           </div>
 
-          <div className={`card-footer`}>
+          <div className={`card-footer bg-zinc-100 rounded-b-lg`}>
             <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Duração: aproximadamente 1 hora e 30 minutos.</p>
           </div>
         </div>
@@ -63,12 +65,12 @@ export default function LeveTarotPage() {
             <Image src={`/04.svg`} width={168} height={120} className={`w-full rounded-t-xl mx-auto`} priority alt="" />
           </div>
 
-          <div className={`card-body bg-zinc-100 h-80`}>
-            <p className={`text-center text-4xl text-zinc-400 pt-1 font-bold square-peg-regular`}>Ciclo Lunar</p>
+          <div className={`card-body bg-zinc-100 h-96`}>
+            <p className={`text-center text-4xl text-zinc-400 pt-1 font-bold square-peg-regular pb-2`}>Ciclo Lunar</p>
             <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Aqui, tiramos as cartas trazendo a previsão do seu ciclo lunar. Olhamos pras 4 fases (lua nova, lua crescente, lua cheia e lua minguante), trazendo a energia de cada uma e observando como as sugestões se relacionam. Excelente pra quem busca clareza, planejamento e ação. É possível fazer algumas perguntas durante a leitura.</p>
           </div>
 
-          <div className={`card-footer`}>
+          <div className={`card-footer bg-zinc-100 rounded-b-lg`}>
             <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Duração: aproximadamente 1 hora e 30 minutos.</p>
           </div>
         </div>
@@ -78,12 +80,12 @@ export default function LeveTarotPage() {
             <Image src={`/05.svg`} width={168} height={120} className={`w-full rounded-t-xl mx-auto`} priority alt="" />
           </div>
 
-          <div className={`card-body bg-zinc-100 h-80`}>
-            <p className={`text-center text-4xl text-zinc-400 pt-1 font-bold square-peg-regular`}>Ano Novo Pessoal</p>
+          <div className={`card-body bg-zinc-100 h-96`}>
+            <p className={`text-center text-4xl text-zinc-400 pt-1 font-bold square-peg-regular pb-2`}>Ano Novo Pessoal</p>
             <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Essa é sua tiragem de aniversário, feita a partir de sua data de aniversário, com a manifestação da energia pra cada um dos meses do seu novo ciclo, até a data do próximo.</p>
           </div>
 
-          <div className={`card-footer`}>
+          <div className={`card-footer bg-zinc-100 rounded-b-lg`}>
             <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Duração: aproximadamente 1 hora e 30 minutos.</p>
           </div>
         </div>
@@ -93,12 +95,12 @@ export default function LeveTarotPage() {
             <Image src={`/06.svg`} width={168} height={120} className={`w-full rounded-t-xl mx-auto`} priority alt="" />
           </div>
 
-          <div className={`card-body bg-zinc-100 h-80`}>
-            <p className={`text-center text-4xl text-zinc-400 pt-1 font-bold square-peg-regular`}>Tarot Astrológico</p>
+          <div className={`card-body bg-zinc-100 h-96`}>
+            <p className={`text-center text-4xl text-zinc-400 pt-1 font-bold square-peg-regular pb-2`}>Tarot Astrológico</p>
             <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Essa é uma leitura mais ampla. Ela traz um olhar sobre seus aspectos naturais, olhando sua energia de ação, material, mental e emocional. E, segue contemplando as 12 casas astrológicas. Cada casa astrológica é representada por um signo, que, por sua vez, manifesta uma temática da nossa vida. Observação: não é mapa astral e não precisa ter mapa astral.</p>
           </div>
 
-          <div className={`card-footer`}>
+          <div className={`card-footer bg-zinc-100 rounded-b-lg`}>
             <p className={`text-center text-zinc-400 px-1 font-pt-mono`}>Duração: aproximadamente 2 horas.</p>
           </div>
         </div>
