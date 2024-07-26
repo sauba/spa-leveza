56d32920f8efab4bf3f2e342624309a391b27f12
Thiago Queiroz <thiagojqueiroz@outlook.com>
2024-07-26 14:56:34 -0300

    Update: About and pages/leveTarot v2

--- src/pages/servicos/tarot/index.tsx
+++ src/pages/servicos/tarot/index.tsx
@@ -19,7 +19,7 @@ export default function LeveTarotPage() {
 
           <div className={`card-body bg-zinc-100 xl:h-96`}>
             <p className={`text-center text-6xl text-zinc-400 pt-1 font-bold square-peg-regular`}>Perguntas</p>
-            <p className={`text-center xl:text-2xl text-zinc-400 px-1 py-8 font-pt-mono`}>Essa tiragem é pra você que tem perguntas específicas. Você pode fazer até 5 perguntas e as cartas serão tiradas nesta energia.</p>
+            <p className={`text-center xl:text-2xl text-zinc-400 px-1 py-4 font-pt-mono`}>Essa tiragem é pra você que tem perguntas específicas. Você pode fazer até 5 perguntas e as cartas serão tiradas nesta energia.</p>
           </div>
 
           <div className={`card-footer bg-zinc-100 rounded-b-lg pb-8 xl:pb-0`}>
@@ -34,7 +34,7 @@ export default function LeveTarotPage() {
 
           <div className={`card-body bg-zinc-100 xl:h-96`}>
             <p className={`text-center text-6xl text-zinc-400 pt-1 font-bold square-peg-regular`}>Áreas da Vida</p>
-            <p className={`text-center xl:text-2xl text-zinc-400 px-1 py-8 font-pt-mono`}>Formato onde tiramos as cartas para leitura das áreas pessoal, profissional, amorosa, familiar, financeira, de saúde e espiritual.</p>
+            <p className={`text-center xl:text-2xl text-zinc-400 px-1 py-4 font-pt-mono`}>Formato onde tiramos as cartas para leitura das áreas pessoal, profissional, amorosa, familiar, financeira, de saúde e espiritual.</p>
           </div>
 
           <div className={`card-footer bg-zinc-100 rounded-b-lg`}>
@@ -49,7 +49,7 @@ export default function LeveTarotPage() {
 
           <div className={`card-body bg-zinc-100 xl:h-96`}>
             <p className={`text-center text-6xl text-zinc-400 pt-1 font-bold square-peg-regular`}>Cruz Celta</p>
-            <p className={`text-center xl:text-2xl text-zinc-400 px-1 py-8 font-pt-mono`}>Aqui, olhamos a fundo uma situação, contemplando o que a atravessa e está posto, o oculto, presente, passado, futuro, você, sentimentos, o externo e possível resultado.</p>
+            <p className={`text-center xl:text-2xl text-zinc-400 px-1 py-4 font-pt-mono`}>Aqui, olhamos a fundo uma situação, contemplando o que a atravessa e está posto, o oculto, presente, passado, futuro, você, sentimentos, o externo e possível resultado.</p>
           </div>
 
           <div className={`card-footer bg-zinc-100 rounded-b-lg`}>
@@ -64,7 +64,7 @@ export default function LeveTarotPage() {
 
           <div className={`card-body bg-zinc-100 xl:h-96`}>
             <p className={`text-center text-6xl text-zinc-400 pt-1 font-bold square-peg-regular`}>Ciclo Lunar</p>
-            <p className={`text-center xl:text-2xl text-zinc-400 px-1 py-8 font-pt-mono`}>Previsão do seu mês lunar, com a energia pra cada fase da lua (nova, crescente, cheia e minguante). Excelente pra quem busca clareza, planejamento e ação.</p>
+            <p className={`text-center xl:text-2xl text-zinc-400 px-1 py-4 font-pt-mono`}>Previsão do seu mês lunar, com a energia pra cada fase da lua (nova, crescente, cheia e minguante). Excelente pra quem busca clareza, planejamento e ação.</p>
           </div>
 
           <div className={`card-footer bg-zinc-100 rounded-b-lg`}>
@@ -79,7 +79,7 @@ export default function LeveTarotPage() {
 
           <div className={`card-body bg-zinc-100 xl:h-96`}>
             <p className={`text-center text-6xl text-zinc-400 pt-1 font-bold square-peg-regular`}>Ano Novo Pessoal</p>
-            <p className={`text-center xl:text-2xl text-zinc-400 px-1 py-8 font-pt-mono`}>Essa é sua tiragem de aniversário. Ela contempla a energia de cada um dos 12 meses do seu novo ciclo.
+            <p className={`text-center xl:text-2xl text-zinc-400 px-1 py-4 font-pt-mono`}>Essa é sua tiragem de aniversário. Ela contempla a energia de cada um dos 12 meses do seu novo ciclo.
               Também se aplica a Ano Novo.</p>
           </div>
 
@@ -95,7 +95,7 @@ export default function LeveTarotPage() {
 
           <div className={`card-body bg-zinc-100 xl:h-96`}>
             <p className={`text-center text-6xl text-zinc-400 pt-1 font-bold square-peg-regular`}>Tarot Astrológico</p>
-            <p className={`text-center xl:text-2xl text-zinc-400 px-1 py-8 font-pt-mono`}>Aqui, fazemos uma mandala com as 12 casas astrológicas, onde cada signo representa um aspecto da vida. São vistos aspectos práticos e profundos.</p>
+            <p className={`text-center xl:text-2xl text-zinc-400 px-1 py-4 font-pt-mono`}>Aqui, fazemos uma mandala com as 12 casas astrológicas, onde cada signo representa um aspecto da vida. São vistos aspectos práticos e profundos.</p>
           </div>
 
           <div className={`card-footer bg-zinc-100 rounded-b-lg`}>
